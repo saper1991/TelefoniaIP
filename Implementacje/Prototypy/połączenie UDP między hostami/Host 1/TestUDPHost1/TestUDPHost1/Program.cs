67126b93533f3e3dcdc56c4179006074dd04cf4c
Damian Lewandowski <damian.lewandowski1991@wp.pl>
2015-03-12 21:38:52 +0100

    Pierwsze prototypy
    Prototyp transmisji danych za pomocą protokołu UDP:
    Host1 wysyla przez ten protokół hostowi 2 ciąg 256 bajtów w celu
    sprawdzenia poprawności kodu i możliwości wykorzystania go w
    przyszłości.

---
+++ Implementacje/Prototypy/połączenie UDP między hostami/Host 1/TestUDPHost1/TestUDPHost1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUDPHost1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient host = new UdpClient(55555);
+
+            try
+            {
+                byte[] bufor = new byte[256];
+                for(int i = 0 ; i < bufor.Length ; i++)
+                {
+                    bufor[i] = (byte)i;
+                }
+                host.Connect("127.0.0.1", 44444);
+                Console.WriteLine("Naciśnij przycisk by rozpocząć transmisję...");
+                Console.ReadKey();
+                host.Send(bufor, bufor.Length);
+                host.Close();
+                Console.WriteLine("Dane zostały przesłane!");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+//wysyłanie pakieów UDP
---
+++ Implementacje/Prototypy/połączenie UDP między hostami/Host 2/TestUDPHost2/TestUDPHost2/TestUDPHost2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUDPHost2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient host = new UdpClient(44444);
+            try
+            {
+                Console.WriteLine("Rozpoczynam odbieranie danych...");
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+                byte[] odebrane = host.Receive(ref ipep);
+
+                for(int i = 0 ; i < odebrane.Length ; i++)
+                {
+                    Console.Write(odebrane[i] + " ");
+                }
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+
+//Odbieranie pakietów UDP
