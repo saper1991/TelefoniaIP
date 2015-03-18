0d1e8f48f6ba54cf9d4bfa29e43c3b6a38b00cba
Damian Lewandowski <damian.lewandowski1991@wp.pl>
2015-03-18 15:29:27 +0100

    Wstępna dokumentacja oraz diagram przypadków użycia

---
+++ Implementacje/Prototypy/Biblioteka NAudio/Tutorial 3/mp3towav Converter/Mp3_to_WaV Converter/Mp3_to_WaV Converter/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Mp3_to_WaV_Converter
+{
+    partial class MainWnd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(12, 12);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(260, 23);
+            this.ConvertButton.TabIndex = 0;
+            this.ConvertButton.Text = "Konwertuj z mp3 do wav";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // MainWnd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 51);
+            this.Controls.Add(this.ConvertButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWnd";
+            this.Text = "Konwerter z mp3 do wav";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertButton;
+    }
+}
+
---
+++ Implementacje/Prototypy/Biblioteka NAudio/Tutorial 3/mp3towav Converter/Mp3_to_WaV Converter/Mp3_to_WaV Converter/Form1.cs
@@ -0,0 +1,60 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mp3_to_WaV_Converter
+{
+    public partial class MainWnd : Form
+    {
+        public MainWnd()
+        {
+            InitializeComponent();
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            string inFile = null, outFile = null;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Plik MP3 (*.mp3) | *.mp3";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                inFile = ofd.FileName;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Plik WAV (.wav)| *.wav";
+
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                outFile = sfd.FileName;
+            }
+
+            if(inFile != null && outFile != null)
+            {
+                using (Mp3FileReader mfr = new Mp3FileReader(inFile))
+                {
+                    using (WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mfr))
+                    {
+                        WaveFileWriter.CreateWaveFile(outFile, pcm);
+                    }
+                }
+                MessageBox.Show("Plik został przekonwertowany!");
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano nazw dla plików");
+            }
+        }
+    }
+}
